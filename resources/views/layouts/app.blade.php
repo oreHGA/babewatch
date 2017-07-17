7dec145446316e2a6a3b58bc16c2e3d6a5a3c3aa
Oreoluwa Ogundipe <oreogundipe@gmail.com>
2017-07-17 09:03:37 +0100

    created views

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title')</title>
+    {{--  Include the necessary bootstrap and jquery here   --}}
+</head>
+<body>
+    @yield('content')
+
+    {{-- Include any necessary JS here   --}}
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Babewatch</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,16 +79,19 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Babewatch 👶🏾
+                </div>
+                <div class="links" style="margin-bottom:20px;">
+                    We watch your baby for you while you are busy.
+                </div> 
+                <div class="links">
+                    <a href="/babe/create">Sign Up</a>
+                    <a href="/babe">Sign In</a>
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <!-- TELL THEM HOW IT WORKS OVER HERE -->
+
+                <!-- Random Question To copywrite it or not ? -->
             </div>
         </div>
     </body>
