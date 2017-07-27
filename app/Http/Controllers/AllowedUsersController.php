07894d955458c19d62483522d7c2cc750cf52306
Oreoluwa Ogundipe <oreogundipe@gmail.com>
2017-07-27 23:35:50 +0100

    added function to add friend

--- app/Http/Controllers/AllowedUsersController.php
+++ app/Http/Controllers/AllowedUsersController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\AllowedUsers;
 
 class AllowedUsersController extends Controller
 {
-    //
+    public function addFriend(Request $r){
+        $username = session('user_email');
+
+        $friend = new AllowedUsers([
+            'firstname' => $r->friend_name,
+            'babe_id' => session('user_id'),
+            'uuid' => uniqid('friend_' . session('user_id') . '_'),
+        ]);
+        $friend->save();
+
+        // TODO: So now the picture has to be sent to the kairo api and saved in the gallery for user
+        
+        return back()->with('status', $friend->firstname . 'has been added');
+    }
 }
