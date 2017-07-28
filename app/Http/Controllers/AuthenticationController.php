c8f8338e130ba84e602c440d89e5b0170422e024
Oreoluwa Ogundipe <oreogundipe@gmail.com>
2017-07-28 02:50:22 +0100

    added sign out

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -10,17 +10,28 @@ class AuthenticationController extends Controller
     // to authenticate users
     public function authenticate(Request $request){
         $babe_model = Babe::where('email', $request->email)->first();
-        $hashed_pass = password_hash($request->password, CRYPT_BLOWFISH);
 
-        if(!$babe_model || $babe_model->password != $hashed_pass)
+        if(!$babe_model || password_verify($request->password, $babe_model->password))
             return back()->with('status', 'Sorry email and password don\'t match');    
         
         // save user in session
         session([
+          'user_name' => $babe_model->username,
           'user_email' => $babe_model->email,
           'user_id' => $babe_model->id,
           'gallery_name' => $babe_model->uuid 
         ]);
-        return view('dashboard');
+        return redirect()->route('dashboard');
+    }
+
+    public function signout(Request $r){
+        // dd(\session()->all());
+        $r->session()->forget([
+            'user_name',
+            'user_email',
+            'user_id',
+            'gallery_name'
+        ]);
+        return redirect()->route('welcome');
     }
 }
