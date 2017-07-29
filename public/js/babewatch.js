9212716e43b02de918f19a828a52e49f091f71b4
Oreoluwa Ogundipe <oreogundipe@gmail.com>
2017-07-29 03:27:36 +0100

    updated js

--- public/js/babewatch.js
+++ public/js/babewatch.js
@@ -13,4 +13,47 @@ $(document).ready(function(){
             $('#pass_error').show();           
         }
     });
+
+    // 
+    function convertURIToImageData(URI) {
+        return new Promise(function(resolve, reject) {
+            if (URI == null) return reject();
+            var canvas = document.createElement('canvas'),
+                context = canvas.getContext('2d'),
+                image = new Image();
+            image.addEventListener('load', function() {
+            canvas.width = image.width;
+            canvas.height = image.height;
+            context.drawImage(image, 0, 0, canvas.width, canvas.height);
+            resolve(context.getImageData(0, 0, canvas.width, canvas.height));
+            }, false);
+            image.src = URI;
+        });
+    }
+
+    // Set up web cam
+    Webcam.set({
+        dest_width: 640,
+        dest_height: 480,
+        image_format: 'png',
+        force_flash: false
+    });
+    Webcam.attach( '#camera' );
+
+    $('#capture').click(function(){
+        Webcam.snap( function(data_uri) {
+            // convertURIToImageData(data_uri).then(function(imageData) {
+                $.ajax({
+                    type: "POST", 
+                    url: "/verifyfriend", 
+                    data:  { 'image' : data_uri , '_token' : CSRF_TOKEN }, 
+                    success: function(result){
+                        console.log(result);
+                    }
+                });
+            // });
+            
+        } );
+
+    });
 });
