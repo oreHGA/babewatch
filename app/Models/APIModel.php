395447492b44cd67d6e0cf00a25c1c1741e5100c
Oreoluwa Ogundipe <oreogundipe@gmail.com>
2017-07-29 03:29:23 +0100

    Updated api caller 😇

--- app/Models/APIModel.php
+++ app/Models/APIModel.php
@@ -31,11 +31,38 @@ class APIModel
         curl_setopt($ch, CURLOPT_HEADER, FALSE);
 
         curl_setopt($ch, CURLOPT_POST, TRUE);
+        
+        curl_setopt($ch, CURLOPT_POSTFIELDS,"{
+        \"image\": " . '"' . $image_url . '"' . ","
+        ." \"subject_id\": " .  '"' .$subject_id . '"' . ","
+        ."\"gallery_name\": " . '"' . $gallery_name . '"'
+        ."}"  );
 
-        curl_setopt($ch, CURLOPT_POSTFIELDS, "{
-        \"image\": " . $image_url . ","
-        ." \"subject_id\": " . $subject_id . ","
-        ."\"gallery_name\": " . $gallery_name
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        "Content-Type: application/json",
+        "app_id: " . $this->app_id,
+        "app_key: " . $this->app_key
+        ));
+
+        $response = json_decode(curl_exec($ch));
+        curl_close($ch);
+        
+        return $response;
+    }
+
+    public function verifyUserFromGallery($image_url, $gallery_name, $subject_id){
+        $url = $this->kairos_url . '/enroll';
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS,"{
+        \"image\": " . '"' . $image_url . '"' . ","
+        ." \"subject_id\": " .  '"' .$subject_id . '"' . ","
+        ."\"gallery_name\": " . '"' . $gallery_name . '"'
         ."}" );
 
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
@@ -44,7 +71,7 @@ class APIModel
         "app_key: " . $this->app_key
         ));
 
-        $response = curl_exec($ch);
+        $response = json_decode(curl_exec($ch));
         curl_close($ch);
 
         return $response;
