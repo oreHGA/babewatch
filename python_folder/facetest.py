ece736f39373a59fb3b2072e97163bc9b45188c2
Oreoluwa Ogundipe <oreogundipe@gmail.com>
2017-07-30 16:34:37 +0100

    remove unused files

--- python_folder/facetest.py
+++
@@ -1,6 +0,0 @@
-import cv2
-import face_recognition
-
-image = face_recognition.load_image_file("OG.jpg")
-print (face_recognition.__version__)
-face_locations = face_recognition.face_locations(image)
